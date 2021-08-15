7e1fa8ded3474dcc7043bb6b5a1023706d0d73a8
Dgtal1 <enderdan80@gmail.com>
2021-08-15 20:48:14 +0300

    Deleted useless "print"

--- examples/WASD_flight.py
+++ examples/WASD_flight.py
@@ -18,7 +18,6 @@ if __name__ == '__main__':
     print('start')
     pioneer_mini = Pioneer()
     while True:
-        print("tid = ", pioneer_mini.get_task_id())
         camera_frame = cv2.imdecode(np.frombuffer(pioneer_mini.get_raw_video_frame(), dtype=np.uint8), cv2.IMREAD_COLOR)
         cv2.imshow('pioneer_camera_stream', camera_frame)
         key = cv2.waitKey(1)
