3290642c59405643cc39eb50efd9f5dd3d68563b
Dgtal1 <helloworld.danil@gmail.com>
2022-02-03 18:19:36 +0300

    Добавлена поддержка трехмерных векторов и точек в существующие функции

--- pioneer_sdk/pioutils.py
+++ pioneer_sdk/pioutils.py
@@ -8,8 +8,13 @@ def vec_from_points(point_start, point_end):
     :param point_end: массив с координатами второй точки
     :return: вектор в виде массива
     """
+    if len(point_start) == 2:
+        point_start = (*point_start, 0)
+    if len(point_end) == 2:
+        point_end = (*point_end, 0)
     return (point_end[0] - point_start[0],
-            point_start[1] - point_end[1])
+            point_start[1] - point_end[1],
+            point_end[2] - point_start[2])
 
 
 def vec_length(vec):
@@ -18,12 +23,14 @@ def vec_length(vec):
     :param vec: вектор в виде массива
     :return: длина вектора
     """
-    return np.sqrt(vec[0] ** 2 + vec[1] ** 2)
+    if len(vec) == 2:
+        vec = (*vec, 0)
+    return np.sqrt(vec[0]**2 + vec[1]**2 + vec[2]**2)
 
 
 def vec_direction(vec, to_degrees=False):
     """
-    Возвращает угол между вектором и горизонтальной осью
+    Возвращает угол между вектором и горизонтальной осью на плоскости изображения
     :param vec: вектор в виде массива
     :param to_degrees: флаг конвертации возвращаемого угла в градусы
     :return: float: угол между вектором и горизонтальной осью
@@ -41,4 +48,8 @@ def distance_between_points(point1, point2):
     :param point2: координаты точки 2
     :return: float: расстояние между точками
     """
-    return np.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+    if len(point1) == 2:
+        point1 = (*point1, 0)
+    if len(point2) == 2:
+        point2 = (*point2, 0)
+    return np.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2 + (point2[2] - point1[2])**2)
