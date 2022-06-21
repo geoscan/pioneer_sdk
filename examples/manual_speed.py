e2a8387adcc7b88a494c4c8c9d28e806072c8f35
DmitryMurashov <dmtr.murashov@gmail.com>
2022-06-21 14:04:43 +0300

    [examples:manual_speed] Ref | Code style

--- examples/manual_speed.py
+++ examples/manual_speed.py
@@ -11,14 +11,14 @@ if __name__ == '__main__':
     while True:
         pioneer_mini.set_manual_speed(vx=0, vy=0, vz=1, yaw_rate=0)
         time.sleep(0.05)
-        if time.time()-t > 2:
+        if time.time() - t > 2:
             break
     time.sleep(4)
     t = time.time()
     while True:
         pioneer_mini.set_manual_speed(vx=1, vy=0, vz=0, yaw_rate=0)
         time.sleep(0.05)
-        if time.time()-t > 3:
+        if time.time() - t > 3:
             break
     time.sleep(4)
     pioneer_mini.land()
