db1a9417eb3417dac2d7d9adb7a612d237772c30
DmitryMurashov <dmtr.murashov@gmail.com>
2022-06-22 14:14:04 +0300

    examples:pioneer_led_blink.lua] Ref | Comments

--- examples/pioneer_led_blink.lua
+++ examples/pioneer_led_blink.lua
@@ -1,24 +1,33 @@
+-- This script serves the demonstration of a "compile/upload" functionality
+-- implemented w/ use of MAVLink FTP subprotocol running on both UAV and
+-- client sides. If the attempt has been successful (it may not be the case
+-- if you use incorrect parameters, or an outdated version of the UAV's
+-- firmware), the demo script will compile, upload, and launch this Lua script.
+-- As a result, the copter will make a short blink.
+--
+-- Please make sure that you use a battery, as USB power supply may not
+-- be sufficient for LEDs.
+
 -- Simplification and caching table.unpack calls
 local unpack = table.unpack
 
--- Base pcb number of RGB LEDs
+-- Both Pioneer and Pioneer Mini have 4 RGB LEDs.
 local ledNumber = 4
--- RGB LED control port initialize
+-- Create an object that encapsulates working w/ LEDs behind a convenient interface
 local leds = Ledbar.new(ledNumber)
 
--- Function changes color on all LEDs
+-- The function sets brightness and color for each of the 4 LEDs
 local function changeColor(color)
-    -- Changing color on each LED one after another
     for i=0, ledNumber - 1, 1 do
         leds:set(i, unpack(color))
     end
 end
 
--- Event processing function called automatically by autopilot
+-- Event processing function where you can handle Autopilot-generated events.
+-- Not needed in this case
 function callback(event)
-
 end
 
-changeColor({.5, .5, .5})
-sleep(1)
-changeColor({0, 0, 0})
+changeColor({.5, .5, .5})  -- Set white color on all LEDs, half of the max brightness
+sleep(1)  -- Hold for a second
+changeColor({0, 0, 0})  -- Disable LEDs
