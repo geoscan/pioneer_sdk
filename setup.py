138c34132842f44e1efc8f3a2c8d605ccfe1cc06
AnastasijaSerova <nastia.efimova88555@gmail.com>
2022-07-14 12:27:28 +0300

    Update version to 0.4.0 Python versio >= 3.10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   name='pioneer_sdk',
   packages=['pioneer_sdk', 'pioneer_sdk.mavsub', 'pioneer_sdk.tools'],
   include_package_data=True,
-  version='0.3.1',
+  version='0.4.0',
   license='MIT',
   description='Programming tools for programming geoscan pioneer drone',
   long_description=long_description,
@@ -26,5 +26,5 @@ setup(
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
   ],
-  python_requires='>=3.6',
+  python_requires='>=3.10',
 )
