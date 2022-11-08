274f779e503b51a55bf4fc979a0eea00b31260db
AnastasijaSerova <a.serova@geoscan.aero>
2022-11-08 17:48:28 +0300

    Change 'case' to 'if'
    1) Change cases to ifs to support older python versions.
    Code checked on:
    python 3.10.5
    python 3.9.0
    python 3.8.0
    python 3.7.0
    
    2) connection_method supports both numeric and verbal arguments

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   name='pioneer_sdk',
   packages=['pioneer_sdk', 'pioneer_sdk.mavsub', 'pioneer_sdk.tools'],
   include_package_data=True,
-  version='0.4.1',
+  version='0.4.2',
   license='MIT',
   description='Programming tools for programming geoscan pioneer drone',
   long_description=long_description,
@@ -29,5 +29,5 @@ setup(
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
   ],
-  python_requires='>=3.10',
+  python_requires='>=3.7',
 )
