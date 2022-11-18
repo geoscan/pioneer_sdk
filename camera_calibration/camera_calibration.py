3b80b2c9b6cf3fb1a11be59b5130d4b3fd522a8c
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-18 09:32:25 +0300

    [camera_calibration:camera_calibration] Ref | Comments, blank lines

--- camera_calibration/camera_calibration.py
+++ camera_calibration/camera_calibration.py
@@ -1,10 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Performs camera calibration using checkerboard pattern. Saves calibration
+values into a file.
+More: https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
+"""
+
 import cv2
 import numpy as np
 import glob
 import time
 from pioneer_sdk import Camera
 
+
 def get_images_from_folder(folder_name, file_type="*.jpg"):
+    """
+    Retreive images from a directory using a provided path (`folder_name`)
+    """
     images_list = glob.glob(folder_name + '/' + file_type)
     images = []
     for fname in images_list:
@@ -12,7 +24,11 @@ def get_images_from_folder(folder_name, file_type="*.jpg"):
         images.append(img)
     return images
 
+
 def get_images_from_drone_camera(camera, save=False):
+    """
+    Retreive images from a drone's camera
+    """
     images = []
     print("Press '1' to take frame\nPress ESC or 'q' to finish")
     while True:
@@ -30,7 +46,11 @@ def get_images_from_drone_camera(camera, save=False):
                 cv2.imwrite('frame_'+str(len(images))+'.jpg', frame)
             time.sleep(1)
 
+
 def get_images_from_computer_camera(capture, save=False):
+    """
+    Retreive images using the PC's camera device
+    """
     images = []
     print("Press '1' to take frame\nPress ESC or 'q' to finish")
     while True:
@@ -53,17 +73,17 @@ def calibrate(images):
     # Defining the dimensions of checkerboard
     CHECKERBOARD = (6,9)
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    
+
     # Creating vector to store vectors of 3D points for each checkerboard image
     objpoints = []
     # Creating vector to store vectors of 2D points for each checkerboard image
-    imgpoints = [] 
-    
-    
+    imgpoints = []
+
+
     # Defining the world coordinates for 3D points
     objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
     objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
-    
+
     # Extracting path of individual image stored in a given directory
     for img in images:
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -75,20 +95,21 @@ def calibrate(images):
             objpoints.append(objp)
             # refining pixel coordinates for given 2d points.
             corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
-            
+
             imgpoints.append(corners2)
-    
+
             # Draw and display the corners
             img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
-        
+
         cv2.imshow('img',img)
         cv2.waitKey(0)
-    
+
     cv2.destroyAllWindows()
-    
+
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    
+
     return mtx, dist
+
 
 def save_coefficients(mtx, dist, path):
     """ Save the camera matrix and the distortion coefficients to given path/file. """
@@ -97,6 +118,7 @@ def save_coefficients(mtx, dist, path):
     cv_file.write("dist", dist)
     # note you *release* you don't close() a FileStorage object
     cv_file.release()
+
 
 def load_coefficients(path):
     """ Loads camera matrix and distortion coefficients. """
