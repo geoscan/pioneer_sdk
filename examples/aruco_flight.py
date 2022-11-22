c75319a93351e849d0a8b4f1d914fc4771702a82
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-22 15:10:31 +0300

    [examples:aruco_flight] Ref | Arithmetic operators spacing, line length

--- examples/aruco_flight.py
+++ examples/aruco_flight.py
@@ -33,19 +33,25 @@ if __name__ == '__main__':
         coordinates = None
         try:
             frame = camera.get_cv_frame()
-            corners, ids, rejected_img_points = cv2.aruco.detectMarkers(frame, aruco_dict, parameters=aruco_parameters)
+            corners, ids, rejected_img_points = cv2.aruco.detectMarkers(
+                frame, aruco_dict, parameters=aruco_parameters)
             # If markers are detected
             if np.all(ids is not None):
                 # Find center of aruco marker on screen
-                x_center = int((corners[0][0][0][0]+ corners[0][0][1][0]+ corners[0][0][2][0]+ corners[0][0][3][0])//4)
-                y_center = int((corners[0][0][0][1]+ corners[0][0][1][1]+ corners[0][0][2][1]+ corners[0][0][3][1])//4)
+                x_center = int((corners[0][0][0][0] + corners[0][0][1][0] \
+                               + corners[0][0][2][0] + corners[0][0][3][0])
+                               // 4)
+                y_center = int((corners[0][0][0][1] + corners[0][0][1][1]
+                               + corners[0][0][2][1] + corners[0][0][3][1])
+                               // 4)
                 dot_size = 5
                 # Draw red square in center of aruco marker
                 frame[y_center - dot_size : y_center + dot_size, x_center - dot_size : x_center + dot_size] = [0,0,255]
                 # Draw markers
                 cv2.aruco.drawDetectedMarkers(frame, corners)
 
-                r_vec_rodrigues, t_vec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, size_of_marker, camera_mtx, camera_dist)
+                r_vec_rodrigues, t_vec, _ = cv2.aruco.estimatePoseSingleMarkers(
+                    corners, size_of_marker, camera_mtx, camera_dist)
                 coordinates = [t_vec.item(0), t_vec.item(1), t_vec.item(2)]
                 # If you need to, you can draw the axes using the following code:
                 # cv2.drawFrameAxes(frame, camera_mtx, camera_dist, r_vec_rodrigues, t_vec, 0.01)
@@ -55,7 +61,8 @@ if __name__ == '__main__':
 
         if coordinates is not None:
             # calculate the distance to the marker
-            distance = np.sqrt(coordinates[0]**2 + coordinates[1]**2 + coordinates[2]**2)
+            distance = np.sqrt(coordinates[0]**2 + coordinates[1]**2
+                               + coordinates[2]**2)
             print(f'Distance: {distance}')
             v_y = 0
             yaw_rate = 0
@@ -65,21 +72,22 @@ if __name__ == '__main__':
             elif distance < 0.5:
                 v_y = -0.4
             # set a non-zero rotation speed if the center of the marker is on the side of the screen
-            if x_center < frame.shape[0]/3:
+            if x_center < frame.shape[0] / 3:
                 yaw_rate = -0.4
-            elif x_center > frame.shape[0]*2/3:
+            elif x_center > frame.shape[0] * 2 / 3:
                 yaw_rate = 0.4
             # if at least one of the values of `v_y` or `yaw_rate` is not 0, send "set_manual_speed()".
             # if not, then send "go_to_local_point_body_fixed()" to stay at this point
             if v_y != 0 or yaw_rate != 0:
-                mini.set_manual_speed_body_fixed(vx=0, vy=v_y, vz=0, yaw_rate=yaw_rate)
+                mini.set_manual_speed_body_fixed(vx=0, vy=v_y, vz=0,
+                                                 yaw_rate=yaw_rate)
                 send_manual_speed = True
             elif send_manual_speed:
                 send_manual_speed = False
                 mini.go_to_local_point_body_fixed(x=0, y=0, z=0, yaw=0)
-        
+
         cv2.imshow('marker_detection', frame) # show frame
-        
+
         key = cv2.waitKey(1)
         if key == 27:  # Exit if the ESC key is pressed
             cv2.destroyAllWindows()
