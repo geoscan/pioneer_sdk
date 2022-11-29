33fd8c758678e714e114f8b3b5a92ef191a1efe2
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-29 12:22:27 +0300

    [examples:ftp] Bug | Removed `list_directory` considering the recently discovered buffer processing issues

--- examples/ftp.py
+++ examples/ftp.py
@@ -7,21 +7,13 @@ from pioneer_sdk.mavsub import ftp as mavftp
 from pioneer_sdk import Pioneer
 
 
-def list_directory(mavlink_connection):
-    ftp_wrapper = mavftp.FtpWrapper(mavlink_connection)
-    print(ftp_wrapper.list_directory("/dev/"))
-
-
 def main():
     drone = Pioneer(logger=True)
-    list_directory(drone.mavlink_socket)
     drone.lua_script_upload("pioneer_led_blink.lua")
     drone.lua_script_control("Start")
     time.sleep(2)
     drone.lua_script_control("Stop")
-    
     drone.close_connection()
-    del drone
 
 
 if __name__ == "__main__":
