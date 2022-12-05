dda4ff18555212f754ddc529af755f692ac73550
AnastasijaSerova <a.serova@geoscan.aero>
2022-12-05 14:55:08 +0300

    Change `circle_flight.py`

--- examples/circle_flight.py
+++ examples/circle_flight.py
@@ -8,14 +8,13 @@ number_of_points = 10
 increment = float(360 / number_of_points)
 radius = 0.6
 flight_height = float(1)
-last_point_reached = False
 
 command_x = radius * math.cos(math.radians(angle))
 command_y = radius * math.sin(math.radians(angle))
 command_yaw = math.radians(angle)
 
-new_point = True
-p_reached = False
+first_point = True
+last_point_reached = False
 
 if __name__ == '__main__':
     print('start')
@@ -24,33 +23,25 @@ if __name__ == '__main__':
     pioneer_mini.takeoff()
     camera = Camera()
     while True:
-        camera_frame = cv2.imdecode(np.frombuffer(camera.get_frame(), dtype=np.uint8), cv2.IMREAD_COLOR)
-        cv2.imshow('pioneer_camera_stream', camera_frame)
+        frame = camera.get_frame()
+        if frame is not None:
+            decoded_frame = cv2.imdecode(np.frombuffer(frame, dtype=np.uint8), cv2.IMREAD_COLOR)
+            cv2.imshow('pioneer_camera_stream', decoded_frame)
 
-        if new_point:
-            pioneer_mini.go_to_local_point(x=command_x, y=command_y, z=flight_height, yaw=command_yaw)
-            new_point = False
-
-        if pioneer_mini.point_reached():
-            p_reached = True
-
-        if p_reached:
-            if angle == 360:
+        if pioneer_mini.point_reached() or first_point:
+            first_point = False
+            if angle >= 360:
                 last_point_reached = True
             else:
                 angle += increment
                 command_x = radius * math.cos(math.radians(angle))
                 command_y = radius * math.sin(math.radians(angle))
                 command_yaw += math.radians(increment)
-                new_point = True
-            p_reached = False
+                pioneer_mini.go_to_local_point(x=command_x, y=command_y, z=flight_height, yaw=command_yaw)
 
         key = cv2.waitKey(1)
         if (key == 27) | last_point_reached:  # esc
             print('esc pressed or mission complete')
             cv2.destroyAllWindows()
             pioneer_mini.land()
-            
-            pioneer_mini.close_connection()
-            del pioneer_mini
-            exit(0)
+            break
