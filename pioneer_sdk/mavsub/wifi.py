81aa62c77f52a60845776aa22d68391721d2bada
DmitryMurashov <dmtr.murashov@gmail.com>
2022-12-07 08:23:48 +0300

    [pioneer_sdk:wifi] Impl | Rewrote 4 legacy support (dbg/tst required)

--- pioneer_sdk/mavsub/wifi.py
+++ pioneer_sdk/mavsub/wifi.py
@@ -10,19 +10,20 @@ Also, for the details of this particular implementation, refer to `./README.md`
 import sys
 import pathlib
 sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent))
-
 import struct
 from ..generic import Logging
 import pymavlink
 from pymavlink.dialects.v20.common import MAVLink_wifi_config_ap_message
 import pymavlink
+import hashlib
 
 RECEIVE_TIMEOUT_SEC = .2
-RECEIVE_ATTEMPTS = 6
+RECEIVE_ATTEMPTS = 20
 RECEIVE_IS_BLOCKING = True
 WIFI_CONFIG_MODE_AP = 1
 WIFI_CONFIG_MODE_STATION = 2
-
+PASSWORD_MAX_LEN = 64
+SSID_MAX_LEN = 32
 
 
 class WifiConfigApMessage(MAVLink_wifi_config_ap_message):
@@ -70,6 +71,9 @@ class Wifi:
     """
     ETL-encapsulating convenience wrapper
     """
+
+    _FIELD_MOCK_PLACEHOLDER = b"\x00\xff"
+
     def __init__(self, connection):
         self.connection = connection
 
@@ -83,8 +87,9 @@ class Wifi:
 
         for _ in range(RECEIVE_ATTEMPTS):
             message_type = "WIFI_CONFIG_AP"
-            msg = self.connection.recv_match(type=message_type, blocking=blocking,
-                                            timeout=timeout)
+            msg = self.connection.recv_match(type=message_type,
+                                             blocking=blocking,
+                                             timeout=timeout)
 
             if not msg:
                 try:
@@ -98,8 +103,141 @@ class Wifi:
 
         return msg
 
-    def send_wifi_config_ap(self, ssid, password, mode):
-        ssid = bytes(ssid, encoding="ascii")
-        password = bytes(password, encoding="ascii")
-        wifi_config_ap_msg = WifiConfigApMessage(ssid, password, mode, 0)
-        self.connection.mav.send(wifi_config_ap_msg, force_mavlink1=False)
+    @staticmethod
+    def _ssid_ensure_format(ssid):
+        """
+        Ensures that SSID is of correct binary format.
+
+        The current implementation uses a set of special sequences. Also, there
+        are subject area limitations on SSID and password lengths.
+        """
+        if ssid is None:
+            ssid = Wifi._FIELD_MOCK_PLACEHOLDER
+        else:
+            ssid = bytes(ssid, encoding="ascii")
+            ssid = ssid[:SSID_MAX_LEN]
+
+    def _password_ensure_format(password):
+        """
+        Ensures that password is of correct binary format.
+
+        The current implementation uses a set of special sequences. Also, there
+        are subject area limitations on SSID and password lengths.
+        """
+        if password is None:
+            password = Wifi._FIELD_MOCK_PLACEHOLDER
+        else:
+            password = password[:PASSWORD_MAX_LEN]
+
+    def send_wifi_config_ap(self, ssid, password):
+        """
+        Basic boilerplate-reducing wrapper. Performs formattings, and
+        ensures consistency w/ the protocol implementation that is being used.
+        """
+        ssid = self._ssid_ensure_format(ssid)
+        password = self._password_ensure_format(password)
+        wifi_config_ap_msg = MAVLink_wifi_config_ap_message(ssid, password)
+        self.connection.mav.send(wifi_config_ap_msg)
+
+    def send_sta_connect(self, ssid, password):
+        self.send_wifi_config_ap(ssid=ssid, password=password)
+
+    def send_sta_disconnect(self, ssid, password):
+        self.send_wifi_config_ap(ssid=None, password=None)
+
+    def send_ap_ssid(self, ssid):
+        self.send_wifi_config_ap(ssid=ssid, password=None)
+
+    def send_ap_password(self, password):
+        self.send_wifi_config_ap(ssid=None, password=password)
+
+    @staticmethod
+    def wifi_config_ap_response_is_ok(wifi_config_ap, ssid, password):
+        """
+        Using the knowledge on the nuances of the protocol implementation,
+        infers whether a response produced by the UAV signifies success or
+        a failure of the request complementary to the response.
+        """
+        assert wifi_config_ap is not None
+        ssid = Wifi._ssid_ensure_format(ssid)
+        password = Wifi._password_ensure_format(password)
+
+        if password != Wifi._FIELD_MOCK_PLACEHOLDER:
+            password = hashlib.md5(password).hexdigest()
+
+        is_ok = wifi_config_ap.ssid == ssid \
+            and wifi_config_ap.password == password
+
+        return is_ok
+
+    @staticmethod
+    def wifi_config_ap_response_connect_is_ok(wifi_config_ap, ssid, password):
+        return Wifi.wifi_config_ap_response_is_ok(wifi_config_ap, ssid=ssid,
+                                                  password=password)
+
+    @staticmethod
+    def wifi_config_ap_response_disconnect_is_ok(wifi_config_ap):
+        return Wifi.wifi_config_ap_response_is_ok(wifi_config_ap, ssid=None,
+                                                  password=None)
+
+    @staticmethod
+    def wifi_config_ap_response_ap_set_ssid_is_ok(wifi_config_ap, ssid):
+        return Wifi.wifi_config_ap_response_is_ok(wifi_config_ap, ssid=ssid,
+                                                  password=None)
+
+    @staticmethod
+    def wifi_config_ap_response_ap_set_password_is_ok(wifi_config_ap, password):
+        return Wifi.wifi_config_ap_response_is_ok(wifi_config_ap, ssid=None,
+                                                  password=password)
+
+    def ap_set_ssid(self, ssid):
+        """
+        Sets the AP's SSID
+        """
+        self.send_ap_ssid(ssid)
+        response = self.receive_wifi_config_ap()
+
+        if response is not None:
+            # TODO request missing message
+            return self.wifi_config_ap_response_ap_set_ssid_is_ok(response)
+
+        return False
+
+    def ap_set_password(self, password):
+        """
+        Sets the AP's password
+        """
+        self.send_ap_password(password)
+        response = self.receive_wifi_config_ap()
+
+        if response is not None:
+            # TODO request missing message
+            return self.wifi_config_ap_response_ap_set_password_is_ok(response)
+
+        return False
+
+    def sta_connect(self, ssid, password):
+        """
+        Makes an attempt to connect the UAV to a remote AP
+        """
+        self.send_sta_connect(ssid, password)
+        response = self.receive_wifi_config_ap()
+
+        if response is not None:
+            # TODO request missing message
+            return self.wifi_config_ap_response_connect_is_ok(response)
+
+        return False
+
+    def sta_disconnect(self):
+        """
+        Disconnects the UAV from whatever AP it happens to be connected to
+        """
+        self.send_sta_disconnect()
+        response = self.receive_wifi_config_ap()
+
+        if response is not None:
+            # TODO request missing message
+            return self.wifi_config_ap_response_disconnect_is_ok(response)
+
+        return False
