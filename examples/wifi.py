0afbdfb285453d90dcb02a2b3c267cba999ba3fd
DmitryMurashov <dmtr.murashov@gmail.com>
2022-12-07 16:18:21 +0300

    [examples:wifi] Impl | Complete connection cycle

--- examples/wifi.py
+++ examples/wifi.py
@@ -2,15 +2,34 @@
 
 import sys
 import pioneer_sdk
+import time
+
+
+AP_SSID = "echothere"
+STA_REMOTE_SSID = "yourssid"
+STA_REMOTE_PASSWORD = "yourpassword"
+
+
+def debug_sleep():
+    time.sleep(2)
 
 
 def test_set_ap_name(drone):
-    drone.set_wifi_ap_settings("echothere", "nopasswordyet")
+    drone.ap_set_ssid(AP_SSID)
+    debug_sleep()
+    print("Successfully set SSID")
 
+    if drone.sta_connect(ssid=STA_REMOTE_SSID, password=STA_REMOTE_PASSWORD):
+        print("Successfully connected")
+        debug_sleep()
+
+        if drone.sta_disconnect():
+            print("Successfully disconnected")
 
 def main():
     drone = pioneer_sdk.piosdk.Pioneer()
     test_set_ap_name(drone)
+    drone.close_connection()
 
 
 if __name__ == "__main__":
