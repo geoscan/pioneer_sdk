5e381de8d0d5b1767032c524ed83f89d03fd0866
DmitryMurashov <dmtr.murashov@gmail.com>
2022-12-08 12:47:24 +0300

    [examples:wifi] Impl | Full-fledged example

--- examples/wifi.py
+++ examples/wifi.py
@@ -6,9 +6,9 @@ import time
 
 
 AP_SSID = "echothere"
-STA_REMOTE_SSID = "yourssid"
-STA_REMOTE_PASSWORD = "yourpassword"
-
+AP_PASSWORD = "password"
+STA_REMOTE_SSID = "thatssid"
+STA_REMOTE_PASSWORD = "password"
 
 def debug_sleep():
     time.sleep(2)
@@ -17,6 +17,10 @@ def test_set_ap_connect_disconnect(drone):
     if drone.ap_set_ssid(AP_SSID):
         debug_sleep()
         print("Successfully set SSID")
+
+    if drone.ap_set_password(AP_PASSWORD):
+        debug_sleep()
+        print("Successfully set password")
 
     if drone.sta_connect(ssid=STA_REMOTE_SSID, password=STA_REMOTE_PASSWORD):
         print("Successfully connected")
