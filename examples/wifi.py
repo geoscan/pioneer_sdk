e07470ccb3135b131b87d93d7634a74a04baadad
DmitryMurashov <dmtr.murashov@gmail.com>
2022-12-12 18:04:00 +0300

    [examples:wifi] Ref | Blank lines, comments

--- examples/wifi.py
+++ examples/wifi.py
@@ -4,20 +4,24 @@ import sys
 import pioneer_sdk
 import time
 
-
+# Access point connection credentials (the UAV is the AP)
 AP_SSID = "echothere"
 AP_PASSWORD = "password"
-STA_REMOTE_SSID = "ssid"
+# Remote access point connection credentials (an access point the UAV attempts to connect to)
+STA_REMOTE_SSID = "somessid"
 STA_REMOTE_PASSWORD = "password"
 
 def debug_sleep():
     time.sleep(2)
 
-def test_set_ap_connect_disconnect(drone):
+def test(drone):
+    # Tweak the values below to test the relevant Wi-Fi functionality
     set_ssid = False
     set_password = False
     connect = False
-    disconnect = True
+    disconnect = False
+    sta_state = False
+    ap_state = True
 
     if set_ssid:
         if drone.ap_set_ssid(AP_SSID):
@@ -38,9 +42,16 @@ def test_set_ap_connect_disconnect(drone):
         if drone.sta_disconnect():
             print("Successfully disconnected")
 
+    if sta_state:
+        print(drone.try_get_sta_status())
+
+    if ap_state:
+        print(drone.try_get_ap_status())
+
+
 def main():
     drone = pioneer_sdk.piosdk.Pioneer()
-    test_set_ap_connect_disconnect(drone)
+    test(drone)
     drone.close_connection()
 
 
