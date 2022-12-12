03b532bfeeced0a9026b9a3473907e1d3075f7f1
DmitryMurashov <dmtr.murashov@gmail.com>
2022-12-12 15:56:16 +0300

    [examples:wifi] Enh | Tweakable execution flow

--- examples/wifi.py
+++ examples/wifi.py
@@ -7,25 +7,34 @@ import time
 
 AP_SSID = "echothere"
 AP_PASSWORD = "password"
-STA_REMOTE_SSID = "thatssid"
+STA_REMOTE_SSID = "ssid"
 STA_REMOTE_PASSWORD = "password"
 
 def debug_sleep():
     time.sleep(2)
 
 def test_set_ap_connect_disconnect(drone):
-    if drone.ap_set_ssid(AP_SSID):
-        debug_sleep()
-        print("Successfully set SSID")
+    set_ssid = False
+    set_password = False
+    connect = False
+    disconnect = True
 
-    if drone.ap_set_password(AP_PASSWORD):
-        debug_sleep()
-        print("Successfully set password")
+    if set_ssid:
+        if drone.ap_set_ssid(AP_SSID):
+            debug_sleep()
+            print("Successfully set SSID")
 
-    if drone.sta_connect(ssid=STA_REMOTE_SSID, password=STA_REMOTE_PASSWORD):
-        print("Successfully connected")
-        debug_sleep()
+    if set_password:
+        if drone.ap_set_password(AP_PASSWORD):
+            debug_sleep()
+            print("Successfully set password")
 
+    if connect:
+        if drone.sta_connect(ssid=STA_REMOTE_SSID, password=STA_REMOTE_PASSWORD):
+            print("Successfully connected")
+            debug_sleep()
+
+    if disconnect:
         if drone.sta_disconnect():
             print("Successfully disconnected")
 
