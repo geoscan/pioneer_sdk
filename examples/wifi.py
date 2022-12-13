96dc3c97b871d6172666655132f3b875232f1f18
DmitryMurashov <dmtr.murashov@gmail.com>
2022-12-13 09:41:16 +0300

    [examples:wifi] Enh | Distinguishable output

--- examples/wifi.py
+++ examples/wifi.py
@@ -14,39 +14,40 @@ STA_REMOTE_PASSWORD = "password"
 def debug_sleep():
     time.sleep(2)
 
+
 def test(drone):
     # Tweak the values below to test the relevant Wi-Fi functionality
     set_ssid = False
     set_password = False
-    connect = False
+    connect = True
     disconnect = False
-    sta_state = False
+    sta_state = True
     ap_state = True
 
     if set_ssid:
         if drone.ap_set_ssid(AP_SSID):
             debug_sleep()
-            print("Successfully set SSID")
+            print("- Successfully set SSID")
 
     if set_password:
         if drone.ap_set_password(AP_PASSWORD):
             debug_sleep()
-            print("Successfully set password")
+            print("- Successfully set password")
 
     if connect:
         if drone.sta_connect(ssid=STA_REMOTE_SSID, password=STA_REMOTE_PASSWORD):
-            print("Successfully connected")
+            print("- Successfully connected")
             debug_sleep()
 
     if disconnect:
         if drone.sta_disconnect():
-            print("Successfully disconnected")
+            print("- Successfully disconnected")
 
     if sta_state:
-        print(drone.try_get_sta_status())
+        print("-", drone.try_get_sta_status())
 
     if ap_state:
-        print(drone.try_get_ap_status())
+        print("-", drone.try_get_ap_status())
 
 
 def main():
